245464de66499d540b5b25e28f18bffad144e63d
TristonBabers <Triston.babers.official@gmail.com>
2022-10-25 16:34:16 -0700

    Did most of the work for expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,49 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+// init is used to define variables after DOM loads
 function init() {
-  // TODO
+  let expose = document.getElementsByClassName("expose");
+  let hornSelection = expose.getElementById("hornSelect");
+  let image = expose.querySelector("[type='img']");
+  let playButton = expose.querySelector("[type='button']");
+  let audio = expose.querySelector("[type='audio']");
 }
+
+//Horn Selection Changed
+hornSelection.addEventListener('change', function(selection) {
+  // Change Image & Audio Based on Selection Value
+  switch (selection.target.value) {
+    case 'air-horn':
+      changeImage(image, assets/images/air-horn.svg); 
+      changeAudio(audio, assets/assets/air-horn.mp3); 
+      break;
+    case 'car-horn':
+      changeImage(image, assets/images/car-horn.svg);
+      changeAudio(audio, assets/assets/car-horn.mp3);
+      break;
+    case 'party-horn':
+      changeImage(image, assets/images/party-horn.svg);
+      changeAudio(audio, assets/assets/party-horn.mp3); 
+      break;
+    default:
+      return;
+  }
+})
+
+// Change Image Source
+function changeImage(img, source) {
+  img.src = source;
+}
+
+// Change Audio Source
+function changeAudio(aux, source) {
+  aux.src = source;
+}
+
+// Play Button is Clicked
+playButton.addEventListener('click', function() {
+
+  // code to run when the event is triggered
+
+}) 
