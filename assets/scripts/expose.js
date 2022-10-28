adc90fa81e6d528fbf88e452d3c19f78a62d0afb
TristonBabers <Triston.babers.official@gmail.com>
2022-10-28 13:47:28 -0700

    Removed debug

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,16 +5,11 @@ window.addEventListener('DOMContentLoaded', init);
 // init is used to define variables after DOM loads
 
 function init() {
-  var expose = document.getElementsByClassName("expose");
-  console.log(expose);        // DELETE ME |||||||||||||||||||||||||||||||||||||||||||
+  //var expose = document.getElementsByClassName("expose");
   var hornSelection = document.getElementById("horn-select");
-  console.log("Horn Selection:" + hornSelection);
   var image = document.querySelector("[type='img']");
-  console.log("image:" + image);        // DELETE ME |||||||||||||||||||||||||||||||||||||||||||
   var playButton = document.querySelector("[type='button']");
-  console.log("playButton:" + playButton);      // DELETE ME |||||||||||||||||||||||||||||||||||||||||||
   var audio = document.querySelector("[type='audio']");
-  console.log("audio:" + audio);      // DELETE ME |||||||||||||||||||||||||||||||||||||||||||
 
   // Event Listeners
   hornSelection.addEventListener('change', hornChangeEvent(selection));
@@ -26,16 +21,16 @@ function hornChangeEvent(selection) {
   // Change Image & Audio Based on Selection Value
   switch (selection.target.value) {
     case 'air-horn':
-      changeImage(image, assets/images/air-horn.svg); 
-      changeAudio(audio, assets/assets/air-horn.mp3);
+      changeImage(image, /assets/images/air-horn.svg); 
+      changeAudio(audio, /assets/assets/air-horn.mp3);
       break;
     case 'car-horn':
-      changeImage(image, assets/images/car-horn.svg);
-      changeAudio(audio, assets/assets/car-horn.mp3);
+      changeImage(image, /assets/images/car-horn.svg);
+      changeAudio(audio, /assets/assets/car-horn.mp3);
       break;
     case 'party-horn':
-      changeImage(image, assets/images/party-horn.svg);
-      changeAudio(audio, assets/assets/party-horn.mp3); 
+      changeImage(image, /assets/images/party-horn.svg);
+      changeAudio(audio, /assets/assets/party-horn.mp3); 
       break;
     default:
       return;
